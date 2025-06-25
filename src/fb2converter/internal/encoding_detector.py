1d027629919aecd7ad59f29f1583f92e44d01abb
KvaytG <vkon2019@inbox.ru>
2025-06-25 21:41:40 +0300

    Minor changes

--- src/fb2converter/internal/encoding_detector.py
+++ src/fb2converter/internal/encoding_detector.py
@@ -3,7 +3,7 @@ from chardet import UniversalDetector
 _detector = UniversalDetector()
 
 
-def detect_encoding(file_path: str, start_position: int, chunk_size: int):
+def detect_encoding(file_path: str, start_position: int, chunk_size: int) -> str:
     """ INTERNAL FUNCTION! """
     with open(file_path, 'rb') as file:
         file.seek(start_position)
